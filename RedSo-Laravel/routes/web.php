c12a72dc050b5b6643cf5587112a77afb248d5da
Maggot11786 <54047089+Maggot11786@users.noreply.github.com>
2020-03-13 21:53:08 -0300

    Add files via upload

---
+++ RedSo-Laravel/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("perfil", function(){
+    return view ("perfil");
+});
+
+
+
+
+
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
