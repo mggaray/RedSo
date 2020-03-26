e14b4881f9252174fff36121d3c4195760972ac2
Alejandro <alemfernandez786@gmail.com>
2020-03-25 22:43:07 -0300

    optimizado estilo y seeder

--- RedSo-Laravel/database/seeds/DatabaseSeeder.php
+++ RedSo-Laravel/database/seeds/DatabaseSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Posteo;
 
 class DatabaseSeeder extends Seeder
+
 {
     /**
      * Seed the application's database.
@@ -12,11 +15,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class); 
-        $seguidos = App\User::All();
 
-        App\User::All()->each(function ($user) use ($seguidos){
+        $usuarios = factory(User::class)->times(50)->create();
+        // $posteos = factory(Posteo::class)->times(50)->create(); 
+
+        $seguidos = User::All();
+
+        User::All()->each(function ($user) use ($seguidos){
             $user->seguidos()->saveMany($seguidos);
          });
+
+
       }
     
 }
