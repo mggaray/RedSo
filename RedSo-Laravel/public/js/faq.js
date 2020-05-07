07262e4096bc1b8b1c645252cbadbf6a0f4fcba8
Alejandro <alemfernandez786@gmail.com>
2020-05-06 21:21:24 -0300

    Agregada funcionalidad preguntas y respuestas

--- RedSo-Laravel/public/js/faq.js
+++ RedSo-Laravel/public/js/faq.js
@@ -1,7 +1,45 @@
-window.onload = function(){
+    function mostrarRespuestas() {
+        let body = document.querySelector("body"); 
+        let respuesta = document.querySelectorAll(".respuesta");
+        let preguntas = document.querySelectorAll(".preguntas");
+        if (body.clientWidth < 500) {
+            
+            for (let index = 0; index < respuesta.length; index++) {                
+                respuesta[index].hidden = true;
+                preguntas[index].style= "cursor: pointer;";  
+            }
+            
+            let pregunta1 = document.querySelector(".pregunta-1");
+            let respuesta1 = document.querySelector(".respuesta-1");
+            pregunta1.onclick = function(){
+            respuesta1.toggleAttribute("hidden")
+            }
+            let pregunta2 = document.querySelector(".pregunta-2");
+            let respuesta2 = document.querySelector(".respuesta-2");
+            pregunta2.onclick = function(){
+            respuesta2.toggleAttribute("hidden")
+            }
+            let pregunta3 = document.querySelector(".pregunta-3");
+            let respuesta3 = document.querySelector(".respuesta-3");
+            pregunta3.onclick = function(){
+            respuesta3.toggleAttribute("hidden")
+            }
+        }
+        else{
+            
+            for (let index = 0; index < respuesta.length; index++) {
+                console.log("qwe");
+                
+                respuesta[index].removeAttribute("hidden");
+                preguntas[index].removeAttribute("style");
+            }
+        }
+    }
 
-    if (document.documentElement.clientWidth > 900) {
-        let respuesta = document.getElementById("respuesta");
-        respuesta.setAttribute("class", "show");
+window.onload = function(){
+        mostrarRespuestas();
+    
+    window.onresize = function(){
+        mostrarRespuestas();
     }
 }
--- RedSo-Laravel/resources/views/faq.blade.php
+++ RedSo-Laravel/resources/views/faq.blade.php
@@ -5,7 +5,41 @@
 @endsection
 
 @section('contenido')
+
 <div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <h1>Preguntas Frecuentes</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam eius officia fugit asperiores explicabo. Neque fuga officiis dolores soluta. Praesentium, delectus voluptatibus maiores qui fugit molestias labore suscipit a earum ab quam in minus excepturi laudantium dignissimos doloribus rem tempora architecto! Adipisci rerum eius asperiores temporibus architecto laboriosam maxime ullam?</p>
+    </div>
+    <div class="col-sm-12 FAQ">
+      <div class="preguntas pregunta-1">
+        <h5>Pregunta 1</h5>
+      </div>
+      <div class="respuesta-1 respuesta" >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod similique vero molestias aliquid, molestiae consequuntur nulla laborum laboriosam accusantium, neque deserunt quia placeat eaque nesciunt possimus, suscipit non repellat? Itaque!</p>
+      </div>
+
+      <div class="preguntas pregunta-2">
+        <h5>Pregunta 2</h5>
+      </div>
+      <div class="respuesta-2 respuesta">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod similique vero molestias aliquid, molestiae consequuntur nulla laborum laboriosam accusantium, neque deserunt quia placeat eaque nesciunt possimus, suscipit non repellat? Itaque!</p>
+      </div>
+
+      <div class="preguntas pregunta-3">
+        <h5>Pregunta 3</h5>
+      </div>
+      <div class="respuesta-3 respuesta">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod similique vero molestias aliquid, molestiae consequuntur nulla laborum laboriosam accusantium, neque deserunt quia placeat eaque nesciunt possimus, suscipit non repellat? Itaque!</p>
+      </div>
+
+    </div>
+
+  </div>
+</div>
+
+{{-- <div class="container">
     <div class="row">
       <div class="col-sm-12">
           <h1>Preguntas frecuentes<h1>
@@ -52,7 +86,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> --}}
 @endsection
 @section('scripts')
 <script src="{{asset('/js/faq.js')}}"></script>
