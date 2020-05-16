809838047e16aed32981c73566588bd3cee1dd75
Alejandro <alemfernandez786@gmail.com>
2020-05-15 21:43:24 -0300

    modif seeder y factory para no generar conflicto

--- RedSo-Laravel/database/seeds/DatabaseSeeder.php
+++ RedSo-Laravel/database/seeds/DatabaseSeeder.php
@@ -7,47 +7,28 @@ use App\Posteo;
 class DatabaseSeeder extends Seeder
 
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+     //
+     // Seed the application's database.
+     //
+     // @return void
     public function run()
     {
-<<<<<<< HEAD
-        // $this->call(UsersTableSeeder::class); 
-
-        $usuarios = factory(User::class)->times(50)->create();
-        // $posteos = factory(Posteo::class)->times(50)->create(); 
-
-        $seguidos = User::All();
-
-        User::All()->each(function ($user) use ($seguidos){
-            $user->seguidos()->saveMany($seguidos);
-         });
-
-
-=======
-        
         // $this->call(UsersTableSeeder::class);  
 
 
-        //$usuarios = factory(App\User::class)->times(499)->create();  EJECUTAR PRIMERO ESTA
+        $usuarios = factory(App\User::class)->times(150)->create();  
+        //EJECUTAR PRIMERO ESTA
 
 
-        //$posteos = factory(App\Posteo::class)->times(490)->create(); LUEGO VERIFICAR EN LA DB EL ID MAX Y MIN DE 
-        //LA TABLA USUARIOS Y MODIFICAR EL POST_FACTORY USER_ID
+        $posteos = factory(App\Posteo::class)->times(150)->create(); 
+        //LUEGO VERIFICAR EN LA DB EL ID MAX Y MIN DE LA TABLA USUARIOS Y MODIFICAR EL POST_FACTORY USER_ID
 
         $seguidos = App\User::All();
 
-       App\User::All()->each(function ($user) use ($seguidos){
-            $user->seguidos()->saveMany($seguidos->random(100));
-<<<<<<< HEAD
-         });*/
->>>>>>> a33b9188d5a5aeb2b5bc3eaa899f1449fb13a571
-=======
-         });
->>>>>>> 096ce91f22684e32e08210a390052a26bb398d7f
-      }
+        App\User::All()->each(function ($user) use ($seguidos){
+            $user->seguidos()->saveMany($seguidos->random(30));
+
+        });
+    }
     
 }
