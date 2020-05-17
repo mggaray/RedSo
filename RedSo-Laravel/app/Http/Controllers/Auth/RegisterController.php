156dc2e8fdaef83b771bd0158d4653b7bc32e1d6
Alejandro <alemfernandez786@gmail.com>
2020-05-17 19:29:55 -0300

    Solucionado error ciudad requerida

--- RedSo-Laravel/app/Http/Controllers/Auth/RegisterController.php
+++ RedSo-Laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
             'apellido' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'], 
-            'ciudad' => ['string', 'max:255'], 
+            'ciudad' => ['nullable', 'string', 'max:255'], 
             'cumpleanios' => ['required','date'],
         ]);
     }
