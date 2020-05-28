82fbf0a8545bd8713b552330102bac0de5f9f0fd
santibruera97 <santibruera97@gmail.com>
2020-05-27 21:37:29 -0300

    bug fix

--- RedSo-Laravel/resources/views/layouts/plantilla_perfil.blade.php
+++ RedSo-Laravel/resources/views/layouts/plantilla_perfil.blade.php
@@ -14,9 +14,16 @@
     </div>
     <div class="info-perfil">
        <br><h1 class="nombre">@yield('usuario') </h1><br>
-       <div class="amigos-bottom">
+       <div class="amigos-bottom"> 
+         @isset($usuarioLogueado)
         <a href="#"><button type="button" class="btn-amigos">Seguidos: <span><b>{{$usuarioLogueado->seguidos->count()}}</b></span></button></a>
-        <a href="#"><button type="button" class="btn-amigos">Seguidores: <span><b>{{$usuarioLogueado->seguidores->count()}} </b></span></button></a> 
+        <a href="#"><button type="button" class="btn-amigos">Seguidores: <span><b>{{$usuarioLogueado->seguidores->count()}} </b></span></button></a>  
+        @endisset 
+
+        @isset($usuario)  
+        <a href="#"><button type="button" class="btn-amigos">Seguidos: <span><b>{{$usuario->seguidos->count()}}</b></span></button></a>
+        <a href="#"><button type="button" class="btn-amigos">Seguidores: <span><b>{{$usuario->seguidores->count()}} </b></span></button></a>  
+        @endisset
       </div>
        <hr>
        <ul class="info-perfil">
