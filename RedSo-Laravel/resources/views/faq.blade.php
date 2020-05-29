8a18b52319f375ece98354f8a32501b1955d5b26
Matias Garay <maty1173@gmail.com>
2020-05-29 16:05:53 -0300

    Listo para la exposicion

--- RedSo-Laravel/resources/views/faq.blade.php
+++ RedSo-Laravel/resources/views/faq.blade.php
@@ -13,26 +13,20 @@
       <p>RedSo es una Red Social sin notificaciones, me gusta ni nada de eso. Un sitio para relajarse y compartir pensamientos, opiniones, anecdotas o lo que quieras!</p>
     </div>
     <div class="col-sm-12 FAQ">
-      <div class="preguntas pregunta-1">
+      <div class="preguntas">
         <h5>¿Por qué no tiene notificaciones ni me gusta?</h5>
       </div>
-      <div class="respuesta-1 respuesta" >
-        <p>Las redes sociales pueden causar estrés, por eso hemos quitado varios de los factoras causantes de ello, como son las notificaciones y me gustas. En RedSo no tienes que preocuparte por ello.</p>
-      </div>
+        <p class="resp">Las redes sociales pueden causar estrés, por eso hemos quitado varios de los factoras causantes de ello, como son las notificaciones y me gustas. En RedSo no tienes que preocuparte por ello.</p>
 
-      <div class="preguntas pregunta-2">
+      <div class="preguntas">
         <h5>¿Qué contenido puedo ver en RedSo?</h5>
       </div>
-      <div class="respuesta-2 respuesta">
-        <p>Puedes seguir a los usuarios que tu quieras y verás solo el contenido de ellos. Así solo verás los posteos de quien tú elijas ver.</p>
-      </div>
+        <p class="resp">Puedes seguir a los usuarios que tu quieras y verás solo el contenido de ellos. Así solo verás los posteos de quien tú elijas ver.</p>
 
-      <div class="preguntas pregunta-3">
+      <div class="preguntas">
         <h5>¿Quién regula qué el contenido de RedSo sea apropiado?</h5>
       </div>
-      <div class="respuesta-3 respuesta">
-        <p>Existen múltiples usuarios administradores que pueden eliminar posteos o comentarios ofensivos o inapropiados.</p>
-      </div>
+        <p class="resp"> Existen múltiples usuarios administradores que pueden eliminar posteos o comentarios ofensivos o inapropiados.</p>
 
     </div>
 
